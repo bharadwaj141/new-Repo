bc88e3d70f74f243f57c64317708a75a5f2b08c1
bharadwaj141 <bharat.bhusan@cloudanalogy.com>
2022-11-24 17:42:17 +0530

    first

---
+++ force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+
+    myname ="hello Friends.";
+
+    connectedCallback(){
+        var act="hello India";
+        window.alert("Action Name :" +act);
+
+       
+}
+    }
+    
